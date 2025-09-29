c16c5cc0fd7efd2ed1a39662cfa497746e4429f3
Victor <ronaldvictoroficial@gmail.com>
2025-09-29 12:25:11 -0300

    first commit of the project

---
+++ script.js
@@ -0,0 +1,19 @@
+// Drawer menu interatividade
+const drawer = document.getElementById('drawer-menu');
+const openBtn = document.querySelector('button .material-symbols-outlined');
+const closeBtn = document.getElementById('close-drawer');
+const menuBtn = document.querySelector('header button');
+
+function openDrawer() {
+    drawer.setAttribute('aria-hidden', 'false');
+    drawer.classList.add('open');
+}
+function closeDrawer() {
+    drawer.setAttribute('aria-hidden', 'true');
+    drawer.classList.remove('open');
+}
+menuBtn.addEventListener('click', openDrawer);
+closeBtn.addEventListener('click', closeDrawer);
+drawer.addEventListener('click', function(e) {
+    if (e.target === drawer) closeDrawer();
+});
